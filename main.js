88649bd3cb8412c6f7c3941d21bd249a771cb7f9
Noah Sprenger <noah.r.sprenger@gmail.com>
2021-10-05 21:23:13 -0400

    I will setup a local enviorment to run this so there is not a overload of commits

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ function validateForm() {
     let x = document.forms["contactForm"]["umail"]
     let y = document.forms["contactForm"]["message"]
     if (x == "") {
-        alert("Email must be filled out")
+        console.log("Email must be filled out")
         return false;
     } 
     if (y == "") {
-        alert("You must enter a message")
+        console.log("You must enter a message")
         return false;
     }
-    window.location.href = "mailto:electrasolutions@electra.ca"+'&subject='+"Inquiry"+'&body='+String(x);
+    window.location.href = "mailto:electrasolutions@electra.ca"+'&subject='+"Inquiry"+'&body='+x.value;
 }
