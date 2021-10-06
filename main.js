e2aecaf344f56299fe833ced6b27fbbab4c133c3
Noah Sprenger <noah.r.sprenger@gmail.com>
2021-10-05 21:08:38 -0400

    Debugging

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function validateForm(form) {
-    // let x = document.forms["contactForm"]["umail"]
-    // let y = document.forms["contactForm"]["message"]
-    let x = form.contactForm.umail
-    let y = form.contactForm.message
+function validateForm() {
+    let x = document.forms["contactForm"]["umail"]
+    let y = document.forms["contactForm"]["message"]
     if (x == "") {
         alert("Email must be filled out")
         return false;
@@ -11,6 +9,9 @@ function validateForm(form) {
         alert("You must enter a message")
         return false;
     }
-    var message = form.message.value    
+    const message = new String(y)   
+    console.log(message)
+    console.log(x)
+    console.log(y)
     location.href = "mailto:electrasolutions@electra.ca"+'&subject='+"Inquiry"+'&body='+message;
 }
