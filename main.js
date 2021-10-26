6e0bf961222d15ed2846bc51dd2d6f6a07b6b85c
Noah Sprenger <noahsprengersonline@gmail.com>
2021-10-25 23:47:36 -0400

    Add language support for contact page and remove unused div in nav bar

--- main.js
+++ main.js
@@ -103,18 +103,30 @@ function french_contact(){
     var t2 = document.getElementById("home");
     var t3 = document.getElementById("about");
     var t4 = document.getElementById("price");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email_prompt");
+    var message = document.getElementById("message_prompt");
+    var send = document.getElementById("send_button");
     // Only need to check if the button has changed
     if (t1.innerHTML == "FR") {
         t1.innerHTML = "EN";
         t2.innerHTML = "Page d'accueil";
         t3.innerHTML = "À Propos";
         t4.innerHTML = "Les Prix";
+        phone.innerHTML = "Numéro de téléphone";
+        email.innerHTML = "Votre Email";
+        message.innerHTML = "Votre Message";
+        send.innerHTML = "Envoyer";
     }
     else {
         t1.innerHTML = "FR";
         t2.innerHTML = "Home";
         t3.innerHTML = "About";
         t4.innerHTML = "Price";
+        phone.innerHTML = "Phone Number";
+        email.innerHTML = "Your Email";
+        message.innerHTML = "Your Message";
+        send.innerHTML = "Submit";
     }
 }
 // Splitting these up into each page
